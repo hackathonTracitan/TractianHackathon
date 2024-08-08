f5d43fe54a1c15316c78f46cf9c4f6368c3a0b58
danie.hosomi <daniel.hosomi@incognia.com>
2024-08-08 11:50:28 -0300

    feat(search): add function to get a list of links

--- src/services/search.py
+++ src/services/search.py
@@ -1,26 +1,30 @@
 import http.client
 import json
 
-conn = http.client.HTTPSConnection("google.serper.dev")
-
-query = "samsung inc"
-
-payload = json.dumps({
-  "q": query,
-  "location": "Brazil",
-  "gl": "br",
-  "hl": "pt-br"
-})
-
-headers = {
+CONN = http.client.HTTPSConnection("google.serper.dev")
+HEADERS = {
   'X-API-KEY': 'ce34436514015496ffd5664d978e68c8b8302efb',
   'Content-Type': 'application/json'
 }
 
-conn.request("POST", "/search", payload, headers)
-res = conn.getresponse()
-data = res.read()
+def get_links(result_dict : dict) -> list:
+    links = []
 
-new_dict = data.decode("utf-8")
+    for result in result_dict["organic"]:
+        links.append(result["link"])
 
-print(type(new_dict))
+    return links
+
+def do_query(query : str) -> list:
+  payload = json.dumps({
+    "q": query,
+    "location": "Brazil",
+    "gl": "br",
+    "hl": "pt-br"
+  })
+
+  CONN.request("POST", "/search", payload, HEADERS)
+
+  query_result = json.loads(CONN.getresponse().read().decode("utf-8"))
+
+  return get_links(query_result)
